5b9b879b1b9888751fad164fd4fa7e616416d3dd
shingokubota <luffa.shingo.cylindrica.gmail.com>
2020-07-05 13:11:57 +0900

    ログインページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get :about,   to: 'static_pages#about'
   get :terms,   to: 'static_pages#terms'
   get :signup,  to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
