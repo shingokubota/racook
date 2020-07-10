2da800fe5e5d26f47eb66634f9e3ac3f9137ba4c
shingokubota <luffa.shingo.cylindrica.gmail.com>
2020-07-10 13:23:54 +0900

    Notificationsモデルの作成

---
+++ db/migrate/20200710032149_create_notifications.rb
@@ -0,0 +1,12 @@
+class CreateNotifications < ActiveRecord::Migration[5.2]
+  def change
+    create_table :notifications do |t|
+      t.integer :user_id
+      t.integer :dish_id
+      t.integer :from_user_id
+
+      t.timestamps
+    end
+    add_index :notifications, :user_id
+  end
+end
