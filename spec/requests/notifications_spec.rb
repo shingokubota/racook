22da9aa5273635fb57f1446c87dc490d904a5b49
shingokubota <luffa.shingo.cylindrica.gmail.com>
2020-07-10 20:03:52 +0900

    通知機能実装・テスト

--- spec/requests/notifications_spec.rb
+++ spec/requests/notifications_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Notifications", type: :request do
   let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
+  let!(:dish) { create(:dish, user: user) }
+  let!(:other_dish) { create(:dish, user: other_user) }
 
   context "通知一覧ページの表示" do
     context "ログインしているユーザーの場合" do
@@ -12,6 +15,21 @@ RSpec.describe "Notifications", type: :request do
       it "レスポンスが正常に表示されること" do
         get notifications_path
         expect(response).to render_template("notifications/index")
+      end
+
+      context "自分以外のユーザーの料理に対して" do
+        it "お気に入り登録によって通知が作成されること" do
+          post "/favorites/#{other_dish.id}/create"
+          expect(user.reload.notification).to be_falsey
+          expect(other_user.reload.notification).to be_truthy
+        end
+      end
+
+      context "自分の料理に対して" do
+        it "お気に入り登録によって通知が作成されないこと" do
+          post "/favorites/#{dish.id}/create"
+          expect(user.reload.notification).to be_falsey
+        end
       end
     end
 
