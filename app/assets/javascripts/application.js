c2cd250e7fdd994013cbd0cb590429eb1e6f3876
shingokubota <luffa.shingo.cylindrica.gmail.com>
2020-07-21 22:29:08 +0900

    css修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,21 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require rails-ujs
 //= require modernizr-2.6.2.min
 //= require activestorage
 //= require turbolinks
+//= require jquery.min
+//= require jquery.easing.1.3
+//= require bootstrap.min
+//= require jquery.waypoints.min
+//= require hoverIntent
+//= require superfish
+//= require jquery.flexslider-min
+//= require jquery.stellar.min
+//= require jquery.stellar.min
+//= require jquery.countTo
+//= require main
+
 // require_tree .
